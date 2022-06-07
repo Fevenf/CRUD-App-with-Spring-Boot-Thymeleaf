3ab54877276fb4982bc1c35bf895fa2b020ac3f8
Fevenf <101144416+Fevenf@users.noreply.github.com>
2022-06-06 21:20:54 -0400

    First commit

---
+++ src/test/java/com/springboot_thymeleaf_crud_web_app/springboot_thymeleaf_crud_web_app/SpringbootThymeleafCrudWebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot_thymeleaf_crud_web_app.springboot_thymeleaf_crud_web_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootThymeleafCrudWebAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
